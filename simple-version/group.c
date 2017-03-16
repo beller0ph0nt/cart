a86f6a4ce181baa8ca611b5ccf79e964c9aaa434
Vershinin Denis <kras_den@rambler.ru>
2017-03-16 22:12:38 +0300

    корректировка группировок

--- simple-version/group.c
+++ simple-version/group.c
@@ -3,37 +3,58 @@
 
 void group_int_num(int* array, int len)
 {
-    int* groups = calloc(len, sizeof(int));
-    int  i      = 0;
-    int  val    = 0;
-    int  min    = array[0];
+    int  i   = 0;
+    int  min = array[0];
+    int  max = min;
 
     for (i = 1; i < len; i++)
     {
-        val = array[i];
+        int val = array[i];
         if (val < min)
-        {
             min = val;
-        }
+        else if (val > max)
+            max = val;
     }
+
+    int  number_of_groups = max - min + 1;
+    int* groups           = calloc(number_of_groups, sizeof(int));
 
     for (i = 0; i < len; i++)
     {
-        groups[array[i]- min]++;
+        int hash = array[i] - min;
+        groups[hash]++;
     }
 
-    for (i = 0; i < len; i++)
+// DEBUG
+    for (i = 0; i < number_of_groups; i++)
     {
          printf("group %d: %d element(s)\n", i + min, groups[i]);
     }
+// DEBUG
 
     free(groups);
 }
 
+#define BITS_IN_BYTE    8
+
+/*
+void group_int_bit_field(int* array, int len)
+{
+    int  number_of_groups = sizeof(int) * BITS_IN_BYTE;
+    int* groups           = calloc(number_of_groups, sizeof(int));
+
+    for (i = 0; i < len; i++)
+    {
+        int hash = 
+    }
+}
+*/
+
+
 int main(int argc, char** argv)
 {
 #define LEN 10
-    int a[LEN] = { 1, 2, 7, 9, 3, 1, 0, 2, 6, 3 };
+    int a[LEN] = { 1, 2, 7, 6, 3, 1, 0, 2, 6, 3 };
 
     group_int_num(a, LEN);
     return 0;
